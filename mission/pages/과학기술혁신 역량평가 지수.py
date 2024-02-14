4ccf4bbc604aabd69d49f5c9970d100c6e7db50a
Jin Geonwoo <jinkonu99@gmail.com>
2024-02-14 14:54:12 +0900

    refactor: 과학기술혁신 역량평가 지수 제목 수정

--- mission/pages/과학기술혁신 역량평가 지수.py
+++ mission/pages/과학기술혁신 역량평가 지수.py
@@ -7,7 +7,7 @@ plt.rc('font', family='AppleGothic')
 df = pd.read_csv('pages/r_costii.csv')
 
 # 차트 제목
-st.title("총 연구개발비의 시도별 추이")
+st.title("지역 과학기술혁신 역량평가 지수")
 selected_regions = st.multiselect('지역 선택', options=df['region'])
 
 data = {}
