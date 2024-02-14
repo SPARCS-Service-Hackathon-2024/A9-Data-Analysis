eeff9e1821b802414b98d9abc51ba0223a493547
Jin Geonwoo <jinkonu99@gmail.com>
2024-02-15 00:39:00 +0900

    refactor: 수정

--- pages/과학기술인 퇴직 준비.py
+++ pages/과학기술인 퇴직 준비.py
@@ -8,8 +8,8 @@ st.title("과학기술인 퇴직 준비")
 
 # 각 연도를 열로 변환하여 데이터 프레임 생성
 df_transposed = df.T
-# df_transposed.columns = df_transposed.iloc[0]
-# df_transposed = df_transposed[0:]
+df_transposed.columns = df_transposed.iloc[0]
+df_transposed = df_transposed[0:]
 
 # 바 차트 그리기
 st.bar_chart(df_transposed)
