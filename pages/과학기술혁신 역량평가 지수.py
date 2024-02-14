fae561de5713d4ca38fcb171bb88a7d946b6a608
14Kgun <geon6757@kaist.ac.kr>
2024-02-14 15:29:34 +0900

    Add: default

--- pages/과학기술혁신 역량평가 지수.py
+++ pages/과학기술혁신 역량평가 지수.py
@@ -10,7 +10,7 @@ df = pd.read_csv('pages/r_costii.csv')
 st.title('지역 과학기술혁신 역량평가 지수')  # 그래프 제목 설정
 
 # CSV 파일의 region 열을 선택지로 사용하여 multiselect 생성
-selected_regions = st.multiselect('지역 선택', options=df['region'])
+selected_regions = st.multiselect('지역 선택', options=df['region'], default=["서울", "대전", "대구", "광주"])
 
 # 선택된 각 지역에 대해 각 연도의 값을 리스트에 저장
 data = {}
