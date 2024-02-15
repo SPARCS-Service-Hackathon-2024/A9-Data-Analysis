916d6b1e7f284a51316e68d858baeaf6f7ca1ed8
14Kgun <geon6757@kaist.ac.kr>
2024-02-15 11:30:05 +0900

    Update fontproperties.py

--- fontproperties.py
+++ fontproperties.py
@@ -1,5 +1,7 @@
 import os
 from matplotlib import font_manager
+from matplotlib import rc
 
 font_path = os.path.join(os.getcwd(), "NanumGothic-Regular.ttf")
 fontprop = font_manager.FontProperties(fname=font_path)
+rc('font', family = 'NanumGothic', size = 14)
